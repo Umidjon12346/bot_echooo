cdc0fec241464b4dc1e6ee3c39ecd1cee659653d
Umidjon12346 <umidshari456@gmail.com>
2024-01-29 18:27:52 +0500

    done

--- bot.py
+++ bot.py
@@ -7,8 +7,12 @@ Token = "6745554947:AAH0Xt-pNF7dasRCeI2BjgMuXN-1o-DzFbk"
 bot = telegram.Bot(Token)
 chat_id = "1806482236"
 
+statust = bot.get_webhook_info()
 
 @app.route("/")
 def user():
-    bot.send_message(chat_id= chat_id,text="Hello world")
-    return "HEll"
+    bot.send_message(chat_id= chat_id,text=str(statust))
+    return "HEllo"
+
+if __name__=="__main__":
+    app.run(debug=True)   
