4893a67c07f7557fa8176aeb641afd338353c5cd
Umidjon12346 <umidshari456@gmail.com>
2024-01-30 15:43:28 +0500

    done

--- bot.py
+++ bot.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 Token = "6745554947:AAH0Xt-pNF7dasRCeI2BjgMuXN-1o-DzFbk"
 bot = telegram.Bot(Token)
+url="https://umidjon12346.pythonanywhere.com/"
+bot.setWebhook(url)
 
 @app.route("/", methods = ["POST"])
 def user():
     data = request.get_json()
-    print(data)
-    bot.send_message(chat_id=data.message.chat.id,text= data.message.text)
+    bot.send_message(chat_id= data.message.chat.id,text= data.message.text)
     return "HElo"
 
 if __name__=="__main__":
